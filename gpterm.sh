49fe51748fe303fd869c32ae15606ea56bebbef1
MadhavShroff <madhavshroff99@gmail.com>
2023-06-09 00:50:53 -0400

    Added support for gpt-4 completions

--- gpterm.sh
+++ gpterm.sh
@@ -2,11 +2,17 @@
 # Script to call
 # Uses jq to parse the JSON response from OpenAI's API
 
+APPEND_SYS_FILE="$HOME/.gpterm_append_sys"
 CONFIG_FILE="$HOME/.gpterm_config"
 
 # Check if config file exists, otherwise create it with a default model
 if [ ! -f "$CONFIG_FILE" ]; then
   echo "text-davinci-003" > "$CONFIG_FILE"
+fi
+
+APPEND_SYS=""
+if [ -f "$APPEND_SYS_FILE" ]; then
+  APPEND_SYS=$(cat "$APPEND_SYS_FILE")
 fi
 
 # Read the current model from the config file
@@ -41,12 +47,36 @@ call_gpt35_turbo() {
   }"
 }
 
-while getopts "m:lh" opt; do
+call_gpt4() {
+  curl -s https://api.openai.com/v1/chat/completions \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $OPENAI_API_KEY" \
+    -d "{
+    \"model\": \"gpt-4\",
+    \"messages\": [
+      {\"role\": \"system\", \"content\": \"You generate ${SHELL##*/} code or commands that perform the task given by the user. You reply with the command string only, with no other text except the command to execute that will successfully perform the task given by the user. Think about the answer step by step, but in the end give the user only the command(s) they need to accomplish their task. ${APPEND_SYS}\"},
+      {\"role\": \"user\", \"content\": \"Compile all Java files in the current directory, then start a new server listening on port 8000 with Main class as the entry point.\"},
+      {\"role\": \"assistant\", \"content\": \"javac *.java;\"},
+      {\"role\": \"user\", \"content\": \"Download this youtube video:https://www.youtube.com/watch?v=Ff4fRgnuFgQ&ab_channel=LexFridman and crop the length from 31:24 to 31:52\"},
+      {\"role\": \"assistant\", \"content\": \"yt-dlp --exec 'ffmpeg -i {} -ss 00:31:24 -to 00:31:52 -c copy {}.mp4' https://www.youtube.com/watch?v=Ff4fRgnuFgQ&ab_channel=LexFridman --keep-video --embed-subs\"},
+      {\"role\": \"user\", \"content\": \"${ARG}\"}
+    ]
+  }"
+}
+
+add_append_sys() {
+  echo "$1" >> "$APPEND_SYS_FILE"
+  echo "Append system prompt successfully added"
+}
+
+while getopts "m:a:lh" opt; do
   case $opt in
     m)
       # check if the model is valid
       if [ "$OPTARG" != "text-davinci-003" ] && 
-         [ "$OPTARG" != "gpt-3.5-turbo" ]; then
+         [ "$OPTARG" != "gpt-3.5-turbo" ] &&
+         [ "$OPTARG" != "gpt-4" ] &&
+         [ "$OPTARG" != "gpt4" ]; then
         echo "ERROR: Invalid model, use -l to list the available models"
         exit 1
       fi
@@ -55,9 +85,14 @@ while getopts "m:lh" opt; do
       echo "Model successfully updated to $MODEL"
       exit 0
       ;;
+    a)
+      add_append_sys "$OPTARG"
+      exit 0
+      ;;
     l)
       echo "text-davinci-003"
       echo "gpt-3.5-turbo"
+      echo "gpt-4"
       exit 0
       ;;
     h|*)
@@ -74,6 +109,8 @@ ARG="$1"
 
 if [ "$MODEL" = "gpt-3.5-turbo" ]; then
   output=$(call_gpt35_turbo)
+elif [ "$MODEL" = "gpt-4" ] || [ "$MODEL" = "gpt4" ]; then
+  output=$(call_gpt4)
 else
   output=$(call_gpt)
 fi
@@ -89,6 +126,8 @@ if [ "$MODEL" = "gpt-3.5-turbo" ]; then
   text=$(echo "$output" | jq -r '.choices[0].message.content')
 elif [ "$MODEL" = "text-davinci-003" ]; then
   text=$(echo "$output" | jq -r '.choices[0].text')
+elif [ "$MODEL" = "gpt-4" ] || [ "$MODEL" = "gpt4" ]; then
+  text=$(echo "$output" | jq -r '.choices[0].message.content')
 fi
 
 # If text is empty or "null", print the output
